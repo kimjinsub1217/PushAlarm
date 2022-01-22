d70162ff5fb349756e14b374d0c107ef58c53b1d
kimjinsub <hikm9985@naver.com>
2022-01-22 19:06:30 +0900

    기본 UI 구성

---
+++ app/src/main/java/com/example/toyproject009_pushalarm/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.toyproject009_pushalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy{
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy{
+        findViewById(R.id.firebaseTokenTextView)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
