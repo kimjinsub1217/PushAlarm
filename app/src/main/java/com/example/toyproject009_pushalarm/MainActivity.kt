061e96a423670aaa8821e86ee4f8f767ca158e87
kimjinsub <hikm9985@naver.com>
2022-01-23 20:23:42 +0900

    firebase  Cloud Messaging 실습해보기

--- app/src/main/java/com/example/toyproject009_pushalarm/MainActivity.kt
+++ app/src/main/java/com/example/toyproject009_pushalarm/MainActivity.kt
@@ -3,18 +3,33 @@ package com.example.toyproject009_pushalarm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
-    private val resultTextView: TextView by lazy{
+    private val resultTextView: TextView by lazy {
         findViewById(R.id.resultTextView)
     }
 
-    private val firebaseToken: TextView by lazy{
+    private val firebaseToken: TextView by lazy {
         findViewById(R.id.firebaseTokenTextView)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFirebase()
+
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+
+            }
     }
 }
